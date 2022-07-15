16799ea30d175c417ea3269511f1162bc0c5310e
Lukas <lukasandel1@gmail.com>
2022-07-15 09:05:29 +0200

    commit

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,28 +1,23 @@
 <link rel="stylesheet" href="/css/navigation.css">
 
-<div class="nav-holder">
-    <nav class="navigation">
-        <a class="navigation__link" href="{{url('/')}}">HOME</a>
-        
-        <a class="navigation__link" href="{{url('/blog')}}">YOUR BLOG</a>
+<div class="admin__nav-holder">
+    <nav class="admin__navigation">
+        <div class="logo"></div>
+        <a class="admin__navigation__link" href="{{url('/')}}">HOME</a>
         @auth
-            {{ Auth::user()->name }}
+            <a class="admin__navigation__link" href="{{url('/blog')}}">YOUR BLOG</a>
+            <a class="admin__navigation__link" href="{{url('/page-creation')}}">PAGE CREATION</a>
+            <a class="admin__navigation__link" href="{{url('/article-creation')}}">ARTICLE CREATION</a>
+        @endauth
+
+        @auth
             <form action="{{ route('logout') }}" method="post">
                 @csrf
-                <button>Log out</button>
+                
+                <button class="btn" style="font-size: 10px"> {{ Auth::user()->name }} Log out</button>
             </form>
-            <a class="navigation__link" href="{{url('/page-creation')}}">PAGE CREATION</a>
-            <a class="navigation__link" href="{{url('/article-creation')}}">ARTICLE CREATION</a>
-        @else
-            <a class="navigation__link" href="{{ route('login') }}">LOGIN</a>
         @endauth
-        
-        @guest
-
-            <a class="navigation__link" href="{{ route('register') }}">REGISTER</a>
-
-        @endguest
-
+        <div class="logo" style="transform: scaleX(-1)"></div>
     </nav>
 </div>
 
