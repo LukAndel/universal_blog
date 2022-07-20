86402b2c5a710da5df82379dfb6a009443339f49
Lukas <lukasandel1@gmail.com>
2022-07-20 10:52:59 +0200

    style

--- resources/js/blog/App.js
+++ resources/js/blog/App.js
@@ -26,7 +26,7 @@ const App = () => {
         <>
         <Router>
             <header>
-                <Navbar />
+                <Navbar user={user}/>
             </header>
             
             <div className="main">
--- resources/views/article/article.blade.php
+++ resources/views/article/article.blade.php
@@ -5,8 +5,8 @@
   <x-head.tinymce-config/>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
-  <link rel="stylesheet" href="css/datepicker/cdnjs.css">
-  <link rel="stylesheet" href="css/datepicker/cdnjs2.css">
+  <link rel="stylesheet" href="/css/datepicker/cdnjs.css">
+  <link rel="stylesheet" href="/css/datepicker/cdnjs2.css">
 @endsection    
 @section('content')        
   <h1 class="title">Article Creation</h1>
